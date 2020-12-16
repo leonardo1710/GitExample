6762d8a82135dfbd4a8d98db53211822991dcd76
Leon Freudenthaler <leon.freudenthaler@stud.fh-campuswien.ac.at>
2020-12-16 12:39:02 +0100

    Added this function. Changed that function. Fixed bug X.

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -2,6 +2,9 @@ package at.ac.fhcampuswien;
 
 public class App {
     public static void main(String[] args){
+        printHelloWorld();
+    }
+    public static void printHelloWorld(){
         System.out.println("Hello World!");
     }
 }
